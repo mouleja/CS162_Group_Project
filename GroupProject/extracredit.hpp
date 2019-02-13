cea0c56e23bd46e48c4da7c0786ec8070f07ccc0
Tommy Armstrong <armstrth@oregonstate.edu>
2019-02-12 22:27:55 -0800

    Updated Simulation so that board has random placing of ants and bugs

---
+++ GroupProject/extracredit.hpp
@@ -0,0 +1,20 @@
+/**********************************************************************************************
+* Document:			Extra Credit File for Group Project
+* Last Modified by:	Tommy Armstrong
+* Group Members:	Tommy Armstrong, Jason Moule, Chetam Prasad, Timothy Winters, Yidong Lin
+* Description:		header file for the extra credit for the group project program.
+					
+***********************************************************************************************/
+
+#ifndef EXTRACREDIT_HPP
+#define EXTRACREDIT_HPP
+
+#include "Critter.h"
+
+void randomPlacing(Critter*** &board, int rows, int cols, int ants, int dbugs);
+
+#endif // !EXTRACREDIT_HPP
+
+
+
+
